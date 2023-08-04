815edfc3130061f68755b0b269051aeec09ba025
Gurjashan <gurjashan-singh@outlook.com>
2023-08-05 01:45:51 +0300

    Complete creating the desktop version of the skills section

---
+++ src/app/components/projects/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./projects.css" ;
+
+
+const Projects = () => {
+
+    return (
+        <>
+            
+        </>
+    )
+  }
+  
+  export default Projects
--- src/app/components/skills/Skills.jsx
+++ src/app/components/skills/Skills.jsx
@@ -3,6 +3,7 @@ import "./skills.css" ;
 
 
 
+
 function importAll(r) {
     let images = {};
     r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
@@ -16,45 +17,95 @@ const Skills = () => {
 
     return (
         <>
-            <div className = "skill__header skill__header--font"> My Skills</div>
+            <div className = "skill__header skill__header--font"> 
+                My Skills 
+            </div>
+            <div className = "skill__header__text skill__header__text--font">
+                Click on each icon to see my experience with each tool / programming language. 
+            </div>
             <div className = "skills__container">
                 <div className = "skills skills--font">
                     <div className = "skill__frontend"> 
-                        <div className = "frontend__header">Frontend </div>
+                        <div className = "frontend__header"> Frontend </div>
                         <div className = "frontend__icons">
-                            <img src = {icons["htmlicon.png"]}/> 
-                            <img src = {icons["cssicon.png"]}/> 
-                            <img src = {icons["javascripticon.png"]}/> 
-                            <img src = {icons["reacticon.png"]}/> 
-                            <img src = {icons["tailwindicon.png"]}/>
-                            <img src = {icons["nextjsicon.png"]}/>
+                            <div className = "icons">
+                                <div><img src = {icons["htmlicon.png"]}/></div>
+                                <div> HTML</div>
+                            </div>
+                            <div className = "icons">
+                                <div><img src = {icons["cssicon.png"]}/></div>
+                                <div> CSS</div>
+                            </div>
+                            <div className = "icons">
+                                <div><img src = {icons["javascripticon.png"]}/></div>
+                                <div> Javascript</div>
+                            </div>
+                            <div className = "icons"> 
+                                <div><img src = {icons["reacticon.png"]}/></div>
+                                <div> React </div>
+                            </div>
+                            <div className = "icons">
+                                <div><img src = {icons["tailwindicon.png"]}/></div>
+                                <div> Tailwind</div>
+                            </div>
+                            <div className = "icons">
+                                <div><img src = {icons["nextjsicon.png"]}/></div>
+                                <div> Nextjs</div>
+                            </div>
                         </div>
                     </div>
                     <div className = "skill__backend"> 
                         <div className = "backend__header"> Backend </div>
-                            <div className = "backend__icons">
-                                <img src = {icons["mysqlicon.png"]}/> 
-                                <img src = {icons["nodeicon.png"]}/> 
+                                <div className = "backend__icons">
+                                <div className = "icons">
+                                    <div><img src = {icons["mysqlicon.png"]}/></div>
+                                    <div> MySQL </div>
+                                </div>
+                                <div className = "icons">
+                                    <div><img src = {icons["nodeicon.png"]}/></div>
+                                    <div> NodeJS</div>
+                                </div>
                             </div>
                         </div>
                     
                     
                     <div className = "skill__tools">
-                        <div className = "other__header"> Tools </div> 
+                        <div className = "tools__header"> Tools </div> 
                             <div className = "tools__icons">
-                                <img src = {icons["webpackicon.png"]}/> 
-                                <img src = {icons["giticon.png"]}/>
-                            </div>    
+                                <div className = "icons">
+                                    <div><img src = {icons["webpackicon.png"]}/></div>
+                                    <div> Webpack </div>
+                                </div>
+                                <div className = "icons">
+                                    <div><img src = {icons["giticon.png"]}/></div>
+                                    <div> Git </div>
+                                </div>
+                            </div>   
                     </div>
                     
                     <div className = "skill__other"> 
                     <div className = "other__header"> Other </div>
                             <div className = "other__icons">
-                                <img src = {icons["c++icon.png"]}/> 
-                                <img src = {icons["cicon.png"]}/>
-                                <img src = {icons["pythonicon.png"]}/> 
-                                <img src = {icons["matlabicon.png"]}/> 
-                                <img src = {icons["verilogicon.png"]}/>
+                                <div className = "icons">
+                                    <div><img src = {icons["pythonicon.png"]}/></div>
+                                    <div> Python </div>
+                                </div>
+                                <div className = "icons">
+                                    <div><img src = {icons["cicon.png"]}/></div>
+                                    <div> C </div>
+                                </div>
+                                <div className = "icons">
+                                    <div><img src = {icons["c++icon.png"]}/></div>
+                                    <div> C++ </div>
+                                </div>
+                                <div className = "icons">
+                                    <div><img src = {icons["verilogicon.png"]}/></div>
+                                    <div> Verilog </div>
+                                </div>
+                                <div className = "icons">
+                                    <div><img src = {icons["matlabicon.png"]}/></div>
+                                    <div> Matlab </div>
+                                </div>
                             </div>
                     </div>
                 </div>
