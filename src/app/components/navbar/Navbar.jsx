7ee8dce0435dca1f711eeddc19327dc6dc43814f
Gurjashan <gurjashan-singh@outlook.com>
2023-09-24 19:02:59 -0400

    Issues in responsiveness of navbar led me to change to a stationary navbar (try to fix this)

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,6 +1,33 @@
 import React from 'react' ;
 import "./navbar.css" ;
 import star from "../../../assets/navbarstar.png"
+import {RxHamburgerMenu, RxCross2} from 'react-icons/rx';
+
+
+const Menu = () =>{
+  return(<>
+    <a href = "#home" className = "navbar__home navbar__link"> 
+        <img src = {star} className = "navbar__icon"/> 
+        <div className = "navbar__link__caption"> Home </div>
+    </a> 
+            
+    <div className = "navbar__skills navbar__link"> 
+      <img src = {star} className = "navbar__icon"/> 
+      <div className = "navbar__link__caption"> Skills </div>
+    </div> 
+            
+    <div className = "navbar__portfolio navbar__link"> 
+      <img src = {star} className = "navbar__icon"/> 
+      <div className = "navbar__link__caption"> Projects </div>
+    </div>
+            
+    <div className = "navbar__contact"> 
+      Contact Me
+    </div>
+    </>
+  )
+}
+
 
 const Navbar = () => {
 
@@ -11,20 +38,20 @@ const Navbar = () => {
         <div className = "navbar navbar--background">
           <div className = "navbar__left navbar__left--font"> jashan. </div>
           <div className= "navbar__right navbar__right--font">
-            <a href = "#home" className = "navbar__home"> 
-             <img src = {star} className = "navbar__icon"/> Home
-            </a> 
-            <div className = "navbar__skills"> 
-             <img src = {star} className = "navbar__icon"/> Skills
-            </div> 
-            <div className = "navbar__portfolio"> 
-             <img src = {star} className = "navbar__icon"/> Projects
-            </div>
-            <div className = "navbar__contact"> 
-              Contact Me
+          
+            <Menu/>
+            {isMenuOpen ? 
+            <div className = "navbar__menu">
+            <div className = "navbar__hamburger_icon" onClick = {() => setMenuState(false)}>
+              <RxCross2 size = {56}/>
             </div>
           </div>
-          
+              : 
+            <div className = "navbar__hamburger_icon" onClick = {() => setMenuState(true)}>
+              <RxHamburgerMenu size = {56}/>
+            </div>
+            }
+          </div>
         </div>
 
   )
