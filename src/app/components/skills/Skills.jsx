caebc8d4b56916a64ce6417737ebffe0009effc9
Gurjashan <gurjashan-singh@outlook.com>
2023-09-25 21:48:14 -0400

    Linked the sections of the website to the navbar and cleaned up code in Skills.jsx

--- src/app/components/skills/Skills.jsx
+++ src/app/components/skills/Skills.jsx
@@ -1,116 +1,166 @@
-import React from 'react';
+import React from "react";
 import "./skills.css";
 
 function importAll(r) {
-    let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images;
+  let images = {};
+  r.keys().map((item, index) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
 }
 
-const icons = importAll(require.context("../../../assets/skillicons", false, /\.(png)$/));
+const icons = importAll(
+  require.context("../../../assets/skillicons", false, /\.(png)$/)
+);
 
+const FrontEndSkills = () => {
+  return (
+    <div className="skill__frontend">
+      <div className="frontend__header"> Frontend </div>
+      <div className="frontend__icons">
+        <div className="icons">
+          <div>
+            <img src={icons["htmlicon.png"]} />
+          </div>
+          <div> HTML </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["cssicon.png"]} />
+          </div>
+          <div> CSS </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["javascripticon.png"]} />
+          </div>
+          <div> Javascript </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["reacticon.png"]} />
+          </div>
+          <div> React </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["tailwindicon.png"]} />
+          </div>
+          <div> Tailwind </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["nextjsicon.png"]} />
+          </div>
+          <div> NextJS </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const BackEndSkills = () => {
+  return (
+    <div className="skill__backend">
+      <div className="backend__header"> Backend </div>
+      <div className="backend__icons">
+        <div className="icons">
+          <div>
+            <img src={icons["mysqlicon.png"]} />
+          </div>
+          <div> MySQL </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["nodeicon.png"]} />
+          </div>
+          <div> NodeJS</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Tools = () => {
+  return (
+    <div className="skill__tools">
+      <div className="tools__header"> Tools </div>
+      <div className="tools__icons">
+        <div className="icons">
+          <div>
+            <img src={icons["webpackicon.png"]} />
+          </div>
+          <div> Webpack </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["giticon.png"]} />
+          </div>
+          <div> Git </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Other = () => {
+  return (
+    <div className="skill__other">
+      <div className="other__header"> Other </div>
+      <div className="other__icons">
+        <div className="icons">
+          <div>
+            <img src={icons["pythonicon.png"]} />
+          </div>
+          <div> Python </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["cicon.png"]} />
+          </div>
+          <div> C </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["c++icon.png"]} />
+          </div>
+          <div> C++ </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["verilogicon.png"]} />
+          </div>
+          <div> Verilog </div>
+        </div>
+        <div className="icons">
+          <div>
+            <img src={icons["matlabicon.png"]} />
+          </div>
+          <div> Matlab </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Skills = () => {
-
-    return (
-        <div id = "skills">
-            <div className="skill__header skill__header--font">
-                My Skills
-            </div>
-            <div className="skill__header__text skill__header__text--font">
-                Click on each icon to see my experience with each tool / programming language.
-            </div>
-            <div className="skills__container">
-                <div className="skills skills--font">
-                    <div className="skill__frontend">
-                        <div className="frontend__header"> Frontend </div>
-                        <div className="frontend__icons">
-                            <div className="icons">
-                                <div><img src={icons["htmlicon.png"]} /></div>
-                                <div> HTML </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["cssicon.png"]} /></div>
-                                <div> CSS </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["javascripticon.png"]} /></div>
-                                <div> Javascript </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["reacticon.png"]} /></div>
-                                <div> React </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["tailwindicon.png"]} /></div>
-                                <div> Tailwind </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["nextjsicon.png"]} /></div>
-                                <div> NextJS </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className="skill__backend">
-                        <div className="backend__header"> Backend </div>
-                        <div className="backend__icons">
-                            <div className="icons">
-                                <div><img src={icons["mysqlicon.png"]} /></div>
-                                <div> MySQL </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["nodeicon.png"]} /></div>
-                                <div> NodeJS</div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className="skill__tools">
-                        <div className="tools__header"> Tools </div>
-                        <div className="tools__icons">
-                            <div className="icons">
-                                <div><img src={icons["webpackicon.png"]} /></div>
-                                <div> Webpack </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["giticon.png"]} /></div>
-                                <div> Git </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="skill__other">
-                        <div className="other__header"> Other </div>
-                        <div className="other__icons">
-                            <div className="icons">
-                                <div><img src={icons["pythonicon.png"]} /></div>
-                                <div> Python </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["cicon.png"]} /></div>
-                                <div> C </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["c++icon.png"]} /></div>
-                                <div> C++ </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["verilogicon.png"]} /></div>
-                                <div> Verilog </div>
-                            </div>
-                            <div className="icons">
-                                <div><img src={icons["matlabicon.png"]} /></div>
-                                <div> Matlab </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="skills">
+      <div className="skill__header skill__header--font">My Skills</div>
+      <div className="skill__header__text skill__header__text--font">
+        Click on each icon to see my experience with each tool / programming
+        language.
+      </div>
+      <div className="skills__container">
+        <div className="skills skills--font">
+          <FrontEndSkills />
+          <BackEndSkills />
+          <Tools />
+          <Other />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
