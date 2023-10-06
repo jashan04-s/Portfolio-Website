49fcaabb47df6a6e563805c10918fbb42d8f1090
Gurjashan <gurjashan-singh@outlook.com>
2023-10-05 22:55:00 -0400

    Added concept code to provide information on each skill

--- src/app/components/aboutskill/Aboutskill.jsx
+++ src/app/components/aboutskill/Aboutskill.jsx
@@ -2,13 +2,32 @@ import React from 'react'
 import "./aboutskill.css" ;
 import { useParams } from 'react-router-dom';
 
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+const icons = importAll(
+  require.context("../../../assets/skillicons", false, /\.(png)$/)
+);
 
 const Aboutskill = () => {
   
   const { SkillName } = useParams();
+  console.log(SkillName)
   return (
-    <div className = "about">
-      
+    <div className = "about--positioner">
+      <div className = "about">
+        <div className = "about__icon">
+          <img src = {icons[SkillName + "icon.png"]}></img>
+        </div>
+        <div className = "about__information">
+          Random Placeholder Content
+        </div>
+        </div>
     </div>
   )
 }
